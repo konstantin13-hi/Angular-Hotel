208df5b605b9738ab4d315bdcf0091964783c620
konstantin13-hi <konstantinchizhevskiy@gmail.com>
2024-06-16 01:02:30 +0200

    added category model and category service(rxjs)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {Button} from "primeng/button";
+import {FaIconLibrary, FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {fontAwesomeIcons} from "./shared/font-awesome-icons";
+import {fab} from "@fortawesome/free-brands-svg-icons";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Button],
+  imports: [RouterOutlet, Button,FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'hotel-angular';
+export class AppComponent implements OnInit {
+  faIconLibrary = inject(FaIconLibrary);
+
+  ngOnInit() {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome() {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
+
+  protected readonly fab = fab;
 }
---
+++ src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,6 @@
+import {IconDefinition} from "@fortawesome/free-regular-svg-icons";
+import {faAirbnb} from "@fortawesome/free-brands-svg-icons";
+
+export const fontAwesomeIcons:IconDefinition[] =[
+      faAirbnb
+]
